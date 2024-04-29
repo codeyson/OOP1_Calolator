7d13d9c055dbcc44a5e5a08094f6c22d6c1f3c2b
Fred <frederickdianzon@gmail.com>
2024-04-30 06:20:32 +0800

    adding main

---
+++ main.py
@@ -0,0 +1,14 @@
+def personal 
+
+
+
+
+def set_goal ():
+    print ("Choose your goal:")
+    print ("1. Gain Weight")
+    print ("2. Maintain Weight")
+    print ("3. Lose Weight")
+    choice = int(input("Enter your choice (1, 2, 3): "))
+
+    if choice = 1:
+        user_data = 
